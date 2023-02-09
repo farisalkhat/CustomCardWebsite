7dd8177eb2ec0aec91833db0cee5f38bbea0b013
Faris Al-khatahtbeh <farisalkhat@gmail.com>
2023-02-09 04:13:08 -0500

    Fixed URL links

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -269,7 +269,7 @@ export class ChecklistComponent implements OnInit {
     
         console.log(new_url)
      
-        window.open(new_url +url, '_blank');
+        window.open(new_url +'/'+url, '_blank');
     
     
         // const newurl = 'https://www.duelingbook.com/card?id='+url
